c0c7dea412f4fd4613dfae2e6ab23812e174d79e
burhansaydin <burhasaydin@gmail.com>
2020-12-01 01:16:24 +0200

    completed

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ from quiz_brain import QuizBrain
 
 question_bank = []
 for i in question_data:
-    q1 = i["text"]
-    a1 = i["answer"]
+    q1 = i["question"]
+    a1 = i["correct_answer"]
     new_q = Question(q1, a1)
     question_bank.append(new_q)
 
 quiz = QuizBrain(question_bank)
 while quiz.still_has_question():
     quiz.next_question()
+
+print("You have completed the quiz.")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
--- quiz_brain.py
+++ quiz_brain.py
@@ -3,15 +3,24 @@ class QuizBrain:
     def __init__(self, q_list):
         self.question_number = 0
         self.question_list = q_list
+        self.score = 0
 
     def still_has_question(self):
-        x = len(self.question_list)
         return self.question_number < len(self.question_list)
 
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
-        input(f"Q. {self.question_number} : {current_question.text} (True/False) : ")
+        reply = input(f"Q. {self.question_number} : {current_question.text} (True/False) : ")
+        self.check_answer(reply, current_question.answer)
 
-
+    def check_answer(self, reply, correct_answer):
+        if reply.lower() == correct_answer.lower():
+            print("You got it right.")
+            self.score += 1
+        else:
+            print("That is wrong.")
+        print(f"The correct answer was: {correct_answer}")
+        print(f"Your current score is: {self.score}/{self.question_number}.")
+        print("\n")
 
